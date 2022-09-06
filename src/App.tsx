e6467458c309e27cc35b3c96796bc4166acc6081
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-09-06 22:25:21 +0200

    build search component, fetch data from geo db to get latitude and longitude of inputsearch country

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
 import { Search } from './components/search/Search';
+import { GEO_API_URL, geoApiOptions } from './api';
 
 function App() {
+
+  const loadOptions = (inputValue: string) => {
+    return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
+      .then(res => res.json())
+      .then(json => {
+        return {
+          options: json.data.map((city: any) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`
+            }
+          })
+        }
+      })
+      .catch(err => console.error('error:' + err));
+  }
+
+  const onHandleSearchChange = (searchData: any) => {
+    console.log(searchData);
+  }
+
   return (
     <div className="container">
-      <Search />
+      <Search
+        loadOptions={loadOptions}
+        onSearchChange={onHandleSearchChange}
+      />
     </div>
   );
 }
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { AsyncPaginate } from 'react-select-async-paginate';
 
-export const Search = () => {
+interface SearchProps {
+    loadOptions: any;
+    onSearchChange: any;
+}
 
-    return(
-        <div>Search</div>
+export const Search: React.FC<SearchProps> = ({ loadOptions, onSearchChange }) => {
+
+    const [search, setSearch] = React.useState<string>('');
+
+    const onHandleChange = (searchData: any) => {
+        setSearch(searchData);
+        onSearchChange(searchData);
+    }
+
+    return (
+        <AsyncPaginate
+            loadOptions={loadOptions}
+            placeholder='Search for the city'
+            debounceTimeout={600}
+            value={search}
+            onChange={onHandleChange}
+        />
     )
 }
