00992d5223e8da893b57d0848f8a0433afaa5576
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-09-06 22:24:07 +0200

    init api.ts file

---
+++ src/api.ts
@@ -0,0 +1,10 @@
+export const GEO_API_URL: string = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd347e19500msh01fe56559b72defp1a7d78jsn897bfe958df2',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
