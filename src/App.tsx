130f1d76968d29e2e82da2e3803979ed66f58624
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2022-09-10 23:19:32 +0200

    add fetch method for current weather and forecast

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Search } from './components/search/Search';
-import { GEO_API_URL, geoApiOptions } from './api';
+import { GEO_API_URL, WEATHER_API_URL, WEATHER_API_KEY, geoApiOptions } from './api';
 import { CurrentWeather } from './components/current-weather/CurrentWeather';
 
 function App() {
+
+  const [currentWeather, setCurrentWeather] = React.useState(null);
+  const [forecastWeather, setForecastWeather] = React.useState(null);
 
   const loadOptions = (inputValue: string) => {
     return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
@@ -23,8 +26,24 @@ function App() {
   }
 
   const onHandleSearchChange = (searchData: any) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(' ');
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+    const forecastWeatherFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+
+    Promise.all([currentWeatherFetch, forecastWeatherFetch])
+      .then(async response => {
+        const currentWeatherResponse = await response[0].json();
+        const forecastWeatherResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...currentWeatherResponse });
+        setForecastWeather({ city: searchData.label, ...forecastWeatherResponse });
+      })
+      .catch(error => console.log(error))
   }
+
+  console.log(currentWeather)
+  console.log(forecastWeather);
 
   return (
     <div className="container">
@@ -32,7 +51,7 @@ function App() {
         loadOptions={loadOptions}
         onSearchChange={onHandleSearchChange}
       />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather />}
     </div>
   );
 }
--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,9 @@
 export const GEO_API_URL: string = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
+export const WEATHER_API_URL: string = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY: string = 'c0b9073f6e9ac5ade25ae344d5b349ff';
+
 export const geoApiOptions = {
     method: 'GET',
     headers: {
@@ -7,4 +11,3 @@ export const geoApiOptions = {
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
-
